ba7b4e595681d1d87c3fa97898fe0413cab3d6e8
Johnnewnham <johnenewnham@gmail.com>
2025-11-04 21:49:52 +0000

    Added pictionary additions.

--- src/app/pictionary/pictionary.component.ts
+++ src/app/pictionary/pictionary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -10,6 +10,12 @@ enum EnabledTab {
 enum ItemType {
 	Character = "Character",
 	Show = "Show",
+}
+
+enum WhatEnabled {
+	Characters = "Characters",
+	Shows = "Shows",
+	All = "All",
 }
 
 class PictionaryItem {
@@ -37,23 +43,61 @@ class PictionaryItem {
 	templateUrl: './pictionary.component.html',
 	styleUrl: './pictionary.component.scss'
 })
-export class PictionaryComponent {
+export class PictionaryComponent implements OnInit {
 	ET = EnabledTab;
 	enabledTab = EnabledTab.Questions;
 
+	WE = WhatEnabled;
+	whatEnabled = WhatEnabled.All;
+
+	allowGuessed = false;
+
 	item1: PictionaryItem = new PictionaryItem(ItemType.Character, "My Hero Academia", "All Might", "https://static.wikia.nocookie.net/bokunoheroacademia/images/c/cd/Toshinori_Yagi_Golden_Age_Hero_Costume_%28Anime%29.png");
 	item2: PictionaryItem = new PictionaryItem(ItemType.Show, "Noragami", undefined, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.crunchyroll.com%2Fseries%2FG6WEV3WM6%2Fnoragami%3Fsrsltid%3DAfmBOorAun43tTaxZkSGBeHOMFMg4uOl9Hnuh9rDCO_6u_U2UrW3caiz&psig=AOvVaw28qyPYzwSxS27CSlVHlM9u&ust=1761675900666000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCIjBoqWAxZADFQAAAAAdAAAAABAE");
+
 	questions: PictionaryItem[] = [this.item1, this.item2];
+	questionsEnabled: PictionaryItem[] = [];
+	questionsUnguessed: PictionaryItem[] = [];
+
+	ngOnInit(): void {
+		this.updateQuestionLists();
+	}
+
+	updateQuestionLists(): void {
+		this.questionsEnabled = this.questions
+			.filter(question => question.enabled == true)
+			.filter(question => {
+				switch (this.whatEnabled) {
+					case WhatEnabled.Characters: return question.type == ItemType.Character;
+					case WhatEnabled.Shows: return question.type == ItemType.Show;
+					default: return true;
+				}
+			});
+
+		this.questionsUnguessed = this.questionsEnabled
+			.filter(question => !question.guessed);
+	}
 
 	setTab(tab: EnabledTab): void {
 		this.enabledTab = tab;
 	}
 
+	setWhatEnabled(enabled: WhatEnabled): void {
+		this.whatEnabled = enabled;
+		this.updateQuestionLists();
+	}
+
+	setAllowGuessed(allowGuessed: boolean): void {
+		this.allowGuessed = allowGuessed;
+	}
+
 	toggleQuestion(index: number): void {
 		this.questions[index].enabled = !this.questions[index].enabled;
+		this.updateQuestionLists();
 	}
 
 	toggleGuessed(index: number): void {
 		this.questions[index].guessed = !this.questions[index].guessed;
+		this.updateQuestionLists();
 	}
 }
