e8c846a5867623ccda69272da9214c83cacd5566
Johnnewnham <johnenewnham@gmail.com>
2025-11-06 23:00:00 +0000

    Added The Chase.

--- src/app/chase/chase.component.ts
+++ src/app/chase/chase.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import questionsJSON from "../../../public/questions.json";
 
 interface ChaseQuestion {
 	question: string;
@@ -6,6 +7,23 @@ interface ChaseQuestion {
 	altAnswer1: string;
 	altAnswer2: string;
 	altAnswer3: string;
+	author?: string;
+	createdAt?: string;
+}
+
+class SelectedQuestion {
+	origin: ChaseQuestion;
+	answers: string[];
+
+	constructor(chaseQuestion: ChaseQuestion) {
+		this.origin = chaseQuestion;
+		this.answers = shuffleArray([
+			chaseQuestion.correctAnswer,
+			chaseQuestion.altAnswer1,
+			chaseQuestion.altAnswer2,
+			chaseQuestion.altAnswer3,
+		]);
+	}
 }
 
 enum BoardTile {
@@ -16,6 +34,39 @@ enum BoardTile {
 	DBlue = "DBlue",
 }
 
+enum Instruction {
+	PlayerTurn = "Please can the contestant select their answer.",
+	ChaserTurn = "Please can the chaser select their answer.",
+}
+
+enum Turn {
+	Player = "Player",
+	Chaser = "Chaser",
+	Reveal = "Reveal",
+	NextQ = "NextQ",
+	GameOver = "GameOver",
+}
+
+function randInt(max: number): number {
+	return Math.floor(Math.random() * max);
+}
+
+function shuffleArray<T>(array: T[]): T[] {
+	// Source - https://stackoverflow.com/questions/60787865/randomize-array-in-angular
+	// Posted by Tzimpo
+	// Retrieved 2025-11-05, License - CC BY-SA 4.0
+	var m = array.length, temp, i;
+
+	while (m) {
+		i = Math.floor(Math.random() * m--);
+		temp = array[m];
+		array[m] = array[i];
+		array[i] = temp;
+	}
+
+	return array;
+}
+
 @Component({
 	selector: 'app-chase',
 	standalone: true,
@@ -23,39 +74,147 @@ enum BoardTile {
 	templateUrl: './chase.component.html',
 	styleUrl: './chase.component.scss'
 })
-export class ChaseComponent {
-	questions: ChaseQuestion[] = [
-		{
-			question: "Which one of these anime weren't referred to as being part of the 'Big 3' Shounen anime in the 2000s-early 2010s?",
-			correctAnswer: "Dragonball",
-			altAnswer1: "Naruto",
-			altAnswer2: "One Piece",
-			altAnswer3: "Bleach",
-		}
-	]
-	selectedQuestion: ChaseQuestion = this.questions[0];
+export class ChaseComponent implements OnInit {
+	T = Turn;
 
-	board: BoardTile[] = [
-		BoardTile.Red,
-		BoardTile.Chaser,
-		BoardTile.LBlue,
-		BoardTile.Player,
-		BoardTile.DBlue,
-		BoardTile.DBlue,
-		BoardTile.DBlue
-	];
+	instruction: string = "Please can the contestant select their answer.";
+
+	questions: ChaseQuestion[] = shuffleArray(questionsJSON.questions);
+	selectedQuestion = new SelectedQuestion(this.questions.at(-1)!);
+	chasers: string[] = [
+		"images/the_chaser_1.png",
+		"images/the_chaser_2.png",
+		"images/the_chaser_3.png",
+	]
+	chaserSelected: string = this.chasers[randInt(this.chasers.length)];
+
+	turn: Turn = Turn.Player;
+
+	board: BoardTile[] = [];
 
 	chaserPosition: number = 0; // 0 is off the board
-	playerPosition: number = 4; // 4 is middle, 4/7. Reach 8 to win
+	playerPosition: number = 3; // 4 is middle, 4/7. Reach 8 to win
 
+	chaserAnswer: number = 4;
+	playerAnswer: number = 4;
+	correctAnswer: number = 4;
+
+	ngOnInit(): void {
+		this.redrawBoard()
+	}
+
+	// Map the BoardTile enum values to the image file paths.
 	getImageRef(tile: BoardTile): string {
 		switch (tile) {
 			case BoardTile.Red: return "images/red.png";
-			case BoardTile.Chaser: return "images/the_chaser_1.png";
+			case BoardTile.Chaser: return this.chaserSelected;
 			case BoardTile.LBlue: return "images/light_blue.png";
 			case BoardTile.Player: return "images/contestant.png";
 			case BoardTile.DBlue: return "images/dark_blue.png";
 			default: return "Invalid enum.";
 		}
+	}
+
+	// Use playerPosition and chaserPosition to redefine board.
+	redrawBoard(): void {
+		let tempBoard: BoardTile[] = [
+			BoardTile.LBlue, BoardTile.LBlue, BoardTile.LBlue, BoardTile.LBlue, BoardTile.LBlue, BoardTile.LBlue, BoardTile.LBlue,
+		]
+
+		if (this.playerPosition < 8) {
+			for (let i = 7; i > this.playerPosition; i--) {
+				tempBoard[i - 1] = BoardTile.DBlue
+			}
+			tempBoard[this.playerPosition - 1] = BoardTile.Player;
+		}
+		if (this.chaserPosition < 8) {
+			for (let i = 1; i < this.chaserPosition; i++) {
+				tempBoard[i - 1] = BoardTile.Red;
+			}
+			tempBoard[this.chaserPosition - 1] = BoardTile.Chaser;
+		} else {
+			tempBoard = [
+				BoardTile.Red, BoardTile.Red, BoardTile.Red, BoardTile.Red, BoardTile.Red, BoardTile.Red, BoardTile.Red,
+			]
+		}
+
+		this.board = tempBoard;
+	}
+
+	// Increment playerPosition by 1 and redraw board.
+	advancePlayer(): void {
+		this.playerPosition++
+		this.redrawBoard()
+	}
+
+	// Increment chaserPosition by 1 and redraw board.
+	advanceChaser(): void {
+		this.chaserPosition++
+		this.redrawBoard()
+	}
+
+	// Reset player/chaserPosition to default and redraw board.
+	resetBoard(): void {
+		this.chaserPosition = 0;
+		this.playerPosition = 3;
+		this.nextQuestion();
+		this.redrawBoard();
+	}
+
+	// To be called when a player selects an answer.
+	submitAnswer(index: number): void {
+		if (this.turn === Turn.Player) {
+			this.playerAnswer = index;
+			this.instruction = "Please can the chaser select their answer.";
+			this.turn = Turn.Chaser;
+			return;
+		}
+
+		if (this.turn === Turn.Chaser) {
+			this.chaserAnswer = index;
+			this.instruction = "Select the button below to reveal the correct answer.";
+			this.turn = Turn.Reveal;
+		}
+	}
+
+	revealAnswer(): void {
+		(this.selectedQuestion.origin.correctAnswer === this.selectedQuestion.answers[this.playerAnswer]) && this.advancePlayer();
+		(this.selectedQuestion.origin.correctAnswer === this.selectedQuestion.answers[this.chaserAnswer]) && this.advanceChaser();
+		this.correctAnswer = this.selectedQuestion.answers.indexOf(this.selectedQuestion.origin.correctAnswer);
+		this.instruction = "Select the button below to go to the next question.";
+		this.turn = Turn.NextQ;
+
+		this.runWinDetection();
+	}
+
+	nextQuestion(): void {
+		this.chaserAnswer = 4;
+		this.playerAnswer = 4;
+		this.correctAnswer = 4;
+		this.instruction = "Please can the contestant select their answer.";
+		this.turn = Turn.Player;
+		this.questions.pop();
+
+		if (this.questions.length === 0) {
+			this.questions = shuffleArray(questionsJSON.questions);
+		}
+
+		this.selectedQuestion = new SelectedQuestion(this.questions.at(-1)!);
+	}
+
+	runWinDetection(): void {
+		if (this.chaserPosition >= this.playerPosition) {
+			this.instruction = "You have been caught and, for you, the chase is over!";
+			this.turn = Turn.GameOver;
+		} else if (this.playerPosition >= 8) {
+			this.instruction = "You have successfully beaten the chaser!";
+			this.turn = Turn.GameOver;
+		}
+	}
+
+	test(): void {
+		console.log("TEST", this.board);
+		// console.log("TEST", randInt(3));
+		// this.redrawBoard()
 	}
 }
